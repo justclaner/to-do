b7050854fbd9829d39d8580707d1c4d4033467e9
justclaner <justclaner@gmail.com>
2024-08-05 14:18:32 -0400

    added all buttons

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -31,15 +31,24 @@ const Home = () => {
             <h1 className="text-center text-3xl my-8">To-do App</h1>
             {/*Insert icon links */}
             </div>
-            {loading ? <Loading /> : <div className='flex justify-content border border-black rounded-lg w-full'>
+            {loading ? <Loading /> : <div className='flex flex-wrap justify-start border border-black rounded-lg w-full'>
                 {projects.map(project=>
                     <div className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
-                        <h1 className='text-2xl'>{project.title}</h1>
+                        <div className="flex flex-wrap items-center justify-start">
+                            <h1 className='text-2xl'>{project.title}</h1>
+                            <button className='border-2 border-black mt-2 p-1 ml-3 px-2 rounded-lg hover:shadow-xl'>Edit Project Title</button>
+                        </div>
+                        
                         {tasks.map(task=>{if(task.projectId == project._id) {
                             return <div className='border-2 border-black px-4 py-2 my-4 rounded-lg'> 
                                 <h2 className='text-xl'>{`Task: ${task.title}`}</h2>
                                 <h3 className="text-lg">{task.description}</h3>
                                 <h3 className="text-lg">{task.date}</h3>
+                                <div className="flex justify-evenly w-full mt-6 mb-2">
+                                <button className='border-2 border-black p-1 px-2 rounded-lg hover:shadow-xl'>Edit Task</button>
+                                <button className='border-2 border-black p-1 px-2 rounded-lg hover:shadow-xl'>Delete Task</button>
+                                </div>
+                                
                                 </div>
                         }}
                         )}
@@ -48,7 +57,9 @@ const Home = () => {
                         <button className='border-2 border-black p-1 w-full mt-6 mb-2 rounded-lg hover:shadow-xl'>Add Task</button>
                         </div>
                 )}
+               
                 </div> } 
+                <button className='border-2 border-black p-1 w-[20%] mt-6 mb-2 rounded-lg hover:shadow-xl'>Add Project</button>
     </div>
   )
 }
